f9fbc89729886edb57098c6858c378bbaccff42f
Mif Masterz <mifmasterz@yahoo.com>
2020-09-26 15:41:51 +0700

    init

---
+++ src/ContainerDetector/BoundingBoxRenderer.cs
@@ -0,0 +1,147 @@
+﻿// Copyright (C) Microsoft Corporation. All rights reserved.
+
+using System.Collections.Generic;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace ContainerDetector
+{
+    /// <summary>
+    /// Helper class to render object detections
+    /// </summary>
+    internal class BoundingBoxRenderer
+    {
+        private Canvas m_canvas;
+
+        // Cache the original Rects we get for resizing purposes
+        private List<BoundingBox> m_rawRects;
+   
+      
+        // Pre-populate rectangles/textblocks to avoid clearing and re-creating on each frame
+        private Rectangle[] m_rectangles;
+        private TextBlock[] m_textBlocks;
+   
+
+      
+        /// <summary>
+        /// </summary>
+        /// <param name="canvas"></param>
+        /// <param name="maxBoxes"></param>
+        /// <param name="lineThickness"></param>
+        /// <param name="colorBrush">Default Colors.SpringGreen color brush if not specified</param>
+        public BoundingBoxRenderer(Canvas canvas, int maxBoxes = 50, int lineThickness = 2, SolidColorBrush colorBrush = null)
+        {
+            m_rawRects = new List<BoundingBox>();
+
+            m_rectangles = new Rectangle[maxBoxes];
+            m_textBlocks = new TextBlock[maxBoxes];
+         
+
+       
+            if (colorBrush == null)
+            {
+                colorBrush = new SolidColorBrush(Colors.SpringGreen);
+            }
+            var lineBrush = new SolidColorBrush(Colors.DarkRed);
+            m_canvas = canvas;
+            for (int i = 0; i < maxBoxes; i++)
+            {
+                // Create rectangles
+                m_rectangles[i] = new Rectangle();
+                // Default configuration
+                m_rectangles[i].Stroke = colorBrush;
+                m_rectangles[i].StrokeThickness = lineThickness;
+                // Hide
+                m_rectangles[i].Visibility = Visibility.Collapsed;
+                // Add to canvas
+                m_canvas.Children.Add(m_rectangles[i]);
+
+                // Create textblocks
+                m_textBlocks[i] = new TextBlock();
+                // Default configuration
+                m_textBlocks[i].Foreground = colorBrush;
+                m_textBlocks[i].FontSize = 18;
+                // Hide
+                m_textBlocks[i].Visibility = Visibility.Collapsed;
+                // Add to canvas
+                m_canvas.Children.Add(m_textBlocks[i]);
+
+             
+
+               
+            }
+        }
+
+        /// <summary>
+        /// Render bounding boxes from ObjectDetections
+        /// </summary>
+        /// <param name="detections"></param>
+        public void Render(IList<PredictionModel> detections)
+        {
+            if (detections == null) return;
+            int i = 0;
+            m_rawRects.Clear();
+            // Render detections up to MAX_BOXES
+            for (i = 0; i < detections.Count && i < m_rectangles.Length; i++)
+            {
+                // Cache rect
+                m_rawRects.Add(detections[i].BoundingBox);
+
+                // Render bounding box
+                m_rectangles[i].Width = detections[i].BoundingBox.Width * m_canvas.ActualWidth;
+                m_rectangles[i].Height = detections[i].BoundingBox.Height * m_canvas.ActualHeight;
+                Canvas.SetLeft(m_rectangles[i], detections[i].BoundingBox.Left * m_canvas.ActualWidth);
+                Canvas.SetTop(m_rectangles[i], detections[i].BoundingBox.Top * m_canvas.ActualHeight);
+                m_rectangles[i].Visibility = Visibility.Visible;
+
+                // Render text label
+                m_textBlocks[i].Text = $"{detections[i].TagName} - {detections[i].Probability * 100}%";
+                Canvas.SetLeft(m_textBlocks[i], detections[i].BoundingBox.Left * m_canvas.ActualWidth + 2);
+                Canvas.SetTop(m_textBlocks[i], detections[i].BoundingBox.Top * m_canvas.ActualHeight + 2);
+                m_textBlocks[i].Visibility = Visibility.Visible;
+            }
+            // Hide all remaining boxes
+            for (; i < m_rectangles.Length; i++)
+            {
+                // Early exit: Everything after i will already be collapsed
+                if (m_rectangles[i].Visibility == Visibility.Collapsed)
+                {
+                    break;
+                }
+                m_rectangles[i].Visibility = Visibility.Collapsed;
+                m_textBlocks[i].Visibility = Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// Resize canvas and rendered bounding boxes
+        /// </summary>
+        public void ResizeContent(SizeChangedEventArgs args)
+        {
+            // Resize rendered bboxes
+            for (int i = 0; i < m_rectangles.Length && m_rectangles[i].Visibility == Visibility.Visible; i++)
+            {
+                // Update bounding box
+                m_rectangles[i].Width = m_rawRects[i].Width * m_canvas.Width;
+                m_rectangles[i].Height = m_rawRects[i].Height * m_canvas.Height;
+                Canvas.SetLeft(m_rectangles[i], m_rawRects[i].Left * m_canvas.Width);
+                Canvas.SetTop(m_rectangles[i], m_rawRects[i].Top * m_canvas.Height);
+
+                // Update text label
+                Canvas.SetLeft(m_textBlocks[i], m_rawRects[i].Left * m_canvas.Width + 2);
+                Canvas.SetTop(m_textBlocks[i], m_rawRects[i].Top * m_canvas.Height + 2);
+
+             
+            }
+          
+          
+        }
+
+
+      
+    }
+}
---
+++ src/ContainerDetector/MainPage.xaml.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ContainerDetector
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            m_bboxRenderer = new BoundingBoxRenderer(UIOverlayCanvas1);
+            // Event handlers
+            //Application.Current.Suspending += ApplicationSuspending;
+            this.Loaded += OnLoaded;
+        }
+
+        private BoundingBoxRenderer m_bboxRenderer = null;
+    
+
+        // Model
+        private ObjectDetection model = null;
+        // File name of the ONNX model. This must be in the Assets folder for the project
+        private string modelFileName = "mask.onnx";
+
+    
+        // Handle property changes
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string score;
+        public string Score
+        {
+            get => this.score;
+            set => this.SetProperty(ref this.score, value);
+        }
+
+        /// <summary>
+        /// Sets <paramref name="propertyName"/> to a <paramref name="value"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            storage = value;
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Handle OnLoad event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+
+            // Load the model
+            await this.LoadModelAsync();
+
+            // Start the camera video preview
+            var availableFrameSourceGroups = await CameraHelper.GetFrameSourceGroupsAsync();
+            if (availableFrameSourceGroups != null)
+            {
+                CameraHelper cameraHelper = new CameraHelper() { FrameSourceGroup = availableFrameSourceGroups.FirstOrDefault() };
+                CamPreview.PreviewFailed += CamPreview_PreviewFailed;
+                await CamPreview.StartAsync(cameraHelper);
+                CamPreview.CameraHelper.FrameArrived += CameraHelper_FrameArrived;
+            }
+
+            //await StartPreviewAsync();
+        }
+
+      
+
+        /// <summary>
+        /// Load and create the model from the .onnx file
+        /// </summary>
+        /// <returns></returns>
+        private async Task LoadModelAsync()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/{modelFileName}"));
+            // Create the model from the file
+            // IMPORTANT: Change `Model.CreateModel` to match the class and methods in the
+            //   .cs file generated from the ONNX model
+            string fname = $@"Assets\{modelFileName}";
+            StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile modelFile = await InstallationFolder.GetFileAsync(fname);
+            if (File.Exists(file.Path))
+            {
+                model = new ObjectDetection(new string[] { "mask", "nomask" });//await ONNXModel.CreateONNXModel(file);
+                await model.Init(modelFile);
+            }
+
+        }
+    
+        /// <summary>
+        /// Process the output returned by the model
+        /// </summary>
+        /// <param name="evalOutput"></param>
+        /// <returns></returns>
+        async Task ProcessOutputAsync(IList<PredictionModel> predictions)//ONNXModelOutput evalOutput
+        {
+            if (predictions != null && predictions.Count > 0)
+            {
+                m_bboxRenderer.Render(predictions);
+            }
+            /*
+            // Get the label and loss from the output
+            string label = evalOutput.classLabel.GetAsVectorView()[0];
+            string loss = (evalOutput.loss[0][label] * 100.0f).ToString("#0.00");
+            // Format the output string
+            string score = $"{label} - {loss}";
+
+            // Display the score
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    this.Score = score;
+                }
+            );*/
+        }
+      
+       
+
+        private async void CameraHelper_FrameArrived(object sender, Microsoft.Toolkit.Uwp.Helpers.FrameEventArgs e)
+        {
+            //if (Interlocked.CompareExchange(ref this.processingFlag, 1, 0) == 0)
+            {
+                try
+                {
+                    var videoFrame = e.VideoFrame;
+
+                    if (videoFrame != null)
+                    {
+                        // If there is a frame, set it as input to the model
+                        //ONNXModelInput input = new ONNXModelInput();
+                        //input.data = videoFrame;
+                        // Evaluate the input data
+                        var evalOutput = await model.PredictImageAsync(videoFrame);
+                        //await model.EvaluateAsync(input);
+                        // Do something with the model output
+                        await this.ProcessOutputAsync(evalOutput);
+                    }
+
+                }
+                finally
+                {
+                    //Interlocked.Exchange(ref this.processingFlag, 0);
+                }
+            }
+        }
+
+      
+
+        private void CamPreview_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            float previewAspectRatio = (float)(CamPreview.ActualWidth / CamPreview.ActualHeight);
+            var cameraAspectRatio = previewAspectRatio;
+            UIOverlayCanvas1.Width = cameraAspectRatio >= previewAspectRatio ? CamPreview.ActualWidth : CamPreview.ActualHeight * cameraAspectRatio;
+            UIOverlayCanvas1.Height = cameraAspectRatio >= previewAspectRatio ? CamPreview.ActualWidth / cameraAspectRatio : CamPreview.ActualHeight;
+
+            m_bboxRenderer.ResizeContent(e);
+        }
+  
+
+      
+
+
+      
+
+        private void CamPreview_PreviewFailed(object sender, Microsoft.Toolkit.Uwp.UI.Controls.PreviewFailedEventArgs e)
+        {
+            var errorMessage = e.Error;
+        }
+
+    }
+}
